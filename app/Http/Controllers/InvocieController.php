f97bdd57d41d75b6a4ff8c0ebae8b260acb80c59
unknown <lorenzosalvadori02@gmail.com>
2023-03-10 14:18:45 +0100

    create models Order, Customer, Invoice w/ ontroller Resource, migration e seeder

---
+++ app/Http/Controllers/InvocieController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invocie;
+use Illuminate\Http\Request;
+
+class InvocieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Invocie $invocie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Invocie $invocie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Invocie $invocie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Invocie $invocie)
+    {
+        //
+    }
+}
